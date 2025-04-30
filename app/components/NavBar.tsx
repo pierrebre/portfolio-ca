8be4f6780404e47824cfba733bdc334f8d05cb37
Pierre Barbé <pierrebarbe0@gmail.com>
2025-04-30 11:24:03 -0400

    init: clean starter project and setup DaisyUI

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
