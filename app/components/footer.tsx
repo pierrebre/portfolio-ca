11103a2d090cca440fda963abeff9fc4805cc003
Pierre Barbé <pierrebarbe0@gmail.com>
2025-05-01 16:39:24 -0400

    feat: enhance Footer component with copyright and navigation links

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,27 @@
 export default function Footer() {
-  return <footer className="border-base-300 bg-base-200 border-t">test</footer>;
+  return (
+    <footer className="border-base-300 bg-base-200 border-t">
+      <div className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="footer footer-vertical sm:footer-horizontal text-base-content py-18 items-end">
+          <aside className="items-center">
+            <p>
+              Pierre Barbé
+              <br />
+              Copyright {new Date().getFullYear()} All rights reserved
+            </p>
+          </aside>
+          <nav>
+            <h4 className="footer-title">Company</h4>
+            <a className="link link-hover" href="/#" >About us</a>
+            <a className="link link-hover" href="/#">Contact</a>
+          </nav>
+          <nav>
+            <h4 className="footer-title">Legal</h4>
+            <a className="link link-hover" href="/#">Terms of use</a>
+            <a className="link link-hover" href="/#">Privacy policy</a>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 }
